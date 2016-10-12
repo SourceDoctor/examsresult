ceae72b9bf298223f481b176acf56806fba0c26c
Thomas Berberich <thomas@walhalla.local>
2016-10-12 18:54:47 +0200

    move Tool functions

--- examsresult/control.py
+++ examsresult/control.py
@@ -1,33 +1,8 @@
-#from .dbhandler import DBHandler
-from configparser import RawConfigParser
+# from .dbhandler import DBHandler
 
-
-def lng_load(language='english', type='ini'):
-    lng_cfg = {}
-
-    if type == 'ini':
-        cfg = RawConfigParser()
-        cfg.read('./lng/%s.lng' % language)
-        for section in cfg.sections():
-            section_dict = {}
-            section_list = cfg.items(section)
-            for k,v in section_list:
-                section_dict[k] = v
-            lng_cfg[section] = section_dict
-
-    return lng_cfg
-
-def center_pos(window_object, width, height):
-    window_object.update_idletasks()
-    w = window_object.winfo_screenwidth()
-    h = window_object.winfo_screenheight()
-    size = tuple(int(_) for _ in window_object.geometry().split('+')[0].split('x'))
-    x = w / 2 - width / 2
-    y = h / 2 - height / 2
-    window_object.geometry("%dx%d+%d+%d" % (size + (x, y)))
 
 class Control(object):
 
     def __init__(self, db_name):
-#        self.db = DBHandler(database=db_name)
+        # self.db = DBHandler(database=db_name)
         pass
---
+++ examsresult/tools.py
@@ -0,0 +1,24 @@
+from configparser import RawConfigParser
+
+
+def lng_load(language='english', type='ini'):
+    lng_cfg = {}
+
+    if type == 'ini':
+        cfg = RawConfigParser()
+        cfg.read('./lng/%s.lng' % language)
+        for section in cfg.sections():
+            section_dict = {}
+            section_list = cfg.items(section)
+            for k, v in section_list:
+                section_dict[k] = v
+            lng_cfg[section] = section_dict
+
+    return lng_cfg
+
+
+def center_pos(window_object, width, height):
+    display = window_object.desktop().screenGeometry()
+    left = (display.width() - width) / 2
+    top = (display.height() - height) / 2
+    return left, top
