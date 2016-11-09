68542746c134396a539029324c467559059519fc
Thomas Berberich <thomas@walhalla.local>
2016-11-09 19:30:56 +0100

    Report Description

--- examsresult/views/report.py
+++ examsresult/views/report.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QWidget, QTableWidget
+from PyQt5.QtWidgets import QWidget, QTableWidget, QLabel
 
 from examsresult.views import CoreView
 
@@ -18,14 +18,50 @@ class ViewReportStudent(ViewReport):
         self.tab_window = root_tab
         self.lng = lng
 
+        self.schoolyear = data['schoolyear']
+        self.schoolclass = data['schoolclass']
+        self.subject = data['subject']
+        self.student_id = data['student_id']
+
         self.column_title = []
         self.column_title.append({'name': 'id', 'type': 'int', 'unique': True, 'editable': False})
         self.column_title.extend(self._define_column_title())
 
         mytab = QWidget()
 
-        self.my_table = QTableWidget(mytab)
+        s = self.dbh.get_student_data(self.student_id)
+        student = "%s, %s" % (s.lastname, s.firstname)
+
+        label_schoolyear_description = QLabel(mytab)
+        label_schoolyear_description.setText(self.lng['schoolyear'])
+        label_schoolyear_description.move(10, 10)
+        label_schoolyear_description = QLabel(mytab)
+        label_schoolyear_description.setText(self.schoolyear)
+        label_schoolyear_description.move(100, 10)
+
+        label_schoolclass_description = QLabel(mytab)
+        label_schoolclass_description.setText(self.lng['schoolclass'])
+        label_schoolclass_description.move(10, 30)
+        label_schoolclass_description = QLabel(mytab)
+        label_schoolclass_description.setText(self.schoolclass)
+        label_schoolclass_description.move(100, 30)
+
+        label_subject_description = QLabel(mytab)
+        label_subject_description.setText(self.lng['subject'])
+        label_subject_description.move(10, 50)
+        label_subject_description = QLabel(mytab)
+        label_subject_description.setText(self.subject)
+        label_subject_description.move(100, 50)
+
+        label_student_description = QLabel(mytab)
+        label_student_description.setText(self.lng['student'])
+        label_student_description.move(10, 70)
+        label_student_description = QLabel(mytab)
+        label_student_description.setText(student)
+        label_student_description.move(100, 70)
+
         self.tab_window.addTab(mytab, self.lng['title'])
+
 
 class ViewReportSchoolclass(ViewReport):
 
@@ -34,11 +70,35 @@ class ViewReportSchoolclass(ViewReport):
         self.tab_window = root_tab
         self.lng = lng
 
+        self.schoolyear = data['schoolyear']
+        self.schoolclass = data['schoolclass']
+        self.subject = data['subject']
+
         self.column_title = []
         self.column_title.append({'name': 'id', 'type': 'int', 'unique': True, 'editable': False})
         self.column_title.extend(self._define_column_title())
 
         mytab = QWidget()
 
-        self.my_table = QTableWidget(mytab)
+        label_schoolyear_description = QLabel(mytab)
+        label_schoolyear_description.setText(self.lng['schoolyear'])
+        label_schoolyear_description.move(10, 10)
+        label_schoolyear_description = QLabel(mytab)
+        label_schoolyear_description.setText(self.schoolyear)
+        label_schoolyear_description.move(100, 10)
+
+        label_schoolclass_description = QLabel(mytab)
+        label_schoolclass_description.setText(self.lng['schoolclass'])
+        label_schoolclass_description.move(10, 30)
+        label_schoolclass_description = QLabel(mytab)
+        label_schoolclass_description.setText(self.schoolclass)
+        label_schoolclass_description.move(100, 30)
+
+        label_subject_description = QLabel(mytab)
+        label_subject_description.setText(self.lng['subject'])
+        label_subject_description.move(10, 50)
+        label_subject_description = QLabel(mytab)
+        label_subject_description.setText(self.subject)
+        label_subject_description.move(100, 50)
+
         self.tab_window.addTab(mytab, self.lng['title'])
