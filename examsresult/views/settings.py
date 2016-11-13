c7d05e75c8eff0d1bb8d0bece39acabdeb63ac6f
Thomas Berberich <thomas@walhalla.local>
2016-11-14 00:09:16 +0100

    configurable calculation Method

--- examsresult/views/settings.py
+++ examsresult/views/settings.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QDialog, QPushButton, QCheckBox, QComboBox, QLabel, QFileDialog
+from PyQt5.QtWidgets import QDialog, QPushButton, QCheckBox, QComboBox, QLabel, QFileDialog, QRadioButton
 
 from examsresult.configuration import current_config, save_config
 from examsresult.tools import lng_list
@@ -9,7 +9,7 @@ class ViewSettings(CoreView):
 
     changed_mark_enabled = False
 
-    def __init__(self, parent, lng, width=400, height=200):
+    def __init__(self, parent, lng, width=700, height=260):
         self.lng = lng
         local_lng = self.lng['window_settings']
 
@@ -23,23 +23,39 @@ class ViewSettings(CoreView):
         self.window.setFixedWidth(width)
         self.window.setWindowTitle(local_lng['title'])
 
-        self.button_save = QPushButton(local_lng['save'], self.window)
-        self.button_save.move(215, 170)
-        self.button_save.clicked.connect(self.save_settings)
+        y = 10
+        # Calculation Methods for Results
+        label_openfile = QLabel(self.lng['calculation_method']['title'], self.window)
+        label_openfile.move(40, y)
+        y += 15
+        self.radio_complete = QRadioButton(self.window)
+        self.radio_complete.move(40, y)
+        self.radio_complete.setText(self.lng['calculation_method']['complete'])
+        self.radio_complete.clicked.connect(self.calculation_method_change)
+        self.radio_timeperiod = QRadioButton(self.window)
+        y += 20
+        self.radio_timeperiod.move(40, y)
+        self.radio_timeperiod.setText(self.lng['calculation_method']['timeperiod'])
+        self.radio_timeperiod.clicked.connect(self.calculation_method_change)
 
-        button_cancel = QPushButton(local_lng['close'], self.window)
-        button_cancel.move(300, 170)
-        button_cancel.clicked.connect(self.window.close)
+        if config['schoolyear_result_calculation_method'] == 'complete':
+            self.radio_complete.setChecked(True)
+        elif config['schoolyear_result_calculation_method'] == 'timeperiod':
+            self.radio_timeperiod.setChecked(True)
+        else:
+            self.radio_complete.setChecked(True)
 
+        y += 35
         # File Openbox on startup ------------------------------------------
         self.checkbox_openbox_on_startup = QCheckBox(local_lng['openbox_on_startup'], self.window)
-        self.checkbox_openbox_on_startup.move(40, 30)
+        self.checkbox_openbox_on_startup.move(40, y)
         self.checkbox_openbox_on_startup.setChecked(config['openbox_on_startup'])
         self.checkbox_openbox_on_startup.stateChanged.connect(self.openbox_on_startup_change)
 
         # language ------------------------------------------
+        y += 30
         self.listbox_language = QComboBox(self.window)
-        self.listbox_language.move(40, 60)
+        self.listbox_language.move(40, y)
         tmp_list = []
         for l in self.language_list:
             tmp_list.append(l[0])
@@ -53,25 +69,39 @@ class ViewSettings(CoreView):
         self.listbox_language.currentIndexChanged.connect(self.language_change)
 
         label_language = QLabel(local_lng['listbox_language'], self.window)
-        label_language.move(130, 64)
+        label_language.move(130, y + 4)
 
         # File to open on startup ------------------------------------------
+        y += 40
         label_openfile = QLabel(local_lng['open_file_on_startup_label'], self.window)
-        label_openfile.move(40, 100)
+        label_openfile.move(40, y)
 
         self.button_select = QPushButton(local_lng['open_file_on_startup_select'], self.window)
-        self.button_select.move(215, 95)
+        self.button_select.move(215, y - 5)
         self.button_select.clicked.connect(self.openfile_on_startup_select)
         
         self.button_clear = QPushButton(local_lng['open_file_on_startup_clear'], self.window)
-        self.button_clear.move(300, 95)
+        self.button_clear.move(300, y - 5)
         self.button_clear.clicked.connect(self.openfile_on_startup_clear)
 
+        y += 20
         self.label_file_to_open = QLabel(config['open_file_on_startup'], self.window)
-        self.label_file_to_open.setGeometry(40, 120, 300, self.label_file_to_open.height())
+        self.label_file_to_open.setGeometry(40, y, 300, self.label_file_to_open.height())
+
+        y += 40
+        button_cancel = QPushButton(local_lng['close'], self.window)
+        button_cancel.move(self.window.width() - button_cancel.width() - 10, y)
+        button_cancel.clicked.connect(self.window.close)
+
+        self.button_save = QPushButton(local_lng['save'], self.window)
+        self.button_save.move(self.window.width() - button_cancel.width() - self.button_save.width() - 10 - 10, y)
+        self.button_save.clicked.connect(self.save_settings)
 
         self.set_changed(False)
         self.window.exec_()
+
+    def calculation_method_change(self):
+        self.set_changed(True)
 
     def language_change(self, index):
         self.set_changed(True)
@@ -103,6 +133,14 @@ class ViewSettings(CoreView):
                 config['language'] = language
                 break
 
+        if self.radio_complete.isChecked():
+            calculation_method = 'complete'
+        elif self.radio_timeperiod.isChecked():
+            calculation_method = 'timeperiod'
+        else:
+            calculation_method = 'complete'
+
+        config['schoolyear_result_calculation_method'] = calculation_method
         config['openbox_on_startup'] = self.checkbox_openbox_on_startup.isChecked()
         config['open_file_on_startup'] = self.label_file_to_open.text()
 
