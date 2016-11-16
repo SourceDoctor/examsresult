26ea3ba1f59b20f06c52857b3a6afdbad2d0c70c
Thomas Berberich <thomas@walhalla.local>
2016-11-16 15:46:24 +0100

    change Result Output from label to table

--- examsresult/views/report.py
+++ examsresult/views/report.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QWidget, QLabel
+from PyQt5.QtWidgets import QWidget, QLabel, QTableWidget
 
 from examsresult import current_config
 from examsresult.views import CoreView
@@ -8,10 +8,10 @@ DIVISOR_PRECISION = 2
 
 class ViewReport(CoreView):
 
-    table_left = 100
+    table_left = 30
     table_top = 130
     table_height = 350
-    table_width = 500
+    table_width = 600
 
     y_pos = 10
 
@@ -19,6 +19,8 @@ class ViewReport(CoreView):
     show_schoolclass = True
     show_subject = True
     show_student = False
+
+    sorting = False
 
     def __init__(self, dbhandler, root_tab, lng, data):
         self.dbh = dbhandler
@@ -75,46 +77,61 @@ class ViewReport(CoreView):
             label_student_description.move(100, self.y_pos)
             self.y_pos += 20
 
-        self.y_pos = self.show_results(root=mytab, y_pos=self.y_pos + 10)
+        self.y_pos += 20
+        self.table_top = self.y_pos
+
+        self.my_table = QTableWidget(mytab)
+        self.my_table.setGeometry(self.table_left, self.table_top, self.table_width, self.table_height)
+
+        self.my_table.setColumnCount(len(self.column_title))
+
+        self.my_table.verticalHeader().setVisible(self.header_vertical)
+        self.my_table.horizontalHeader().setVisible(self.header_horizontal)
+
+        column_tuple = ()
+        for col in self.column_title:
+            column_tuple += (col['name'],)
+        self.my_table.setHorizontalHeaderLabels(column_tuple)
+
+        self.load_data()
+
+        self.my_table.resizeColumnsToContents()
+        self.my_table.setSortingEnabled(self.sorting)
 
         self.tab_window.addTab(mytab, self.lng['title'])
-
-    def show_results(self, root, y_pos=100):
-        return y_pos
-
-    def print_content(self, root, y, data):
-
-        def print_label(root, x, y, text):
-            label_date = QLabel(root)
-            label_date.setText(text)
-            label_date.move(x, y)
-
-        x_coord = 70
-        for text, x in data:
-            x_coord += x
-            print_label(root, x_coord, y, str(text))
 
 
 class ViewReportStudent(ViewReport):
 
     show_student = True
 
-    def show_results(self, root, y_pos=100):
+    def _define_column_title(self):
+        return [{'name': self.lng['date'], 'type': 'string', 'unique': False},
+                {'name': self.lng['timeperiod'], 'type': 'string', 'unique': False},
+                {'name': self.lng['examtype'], 'type': 'string', 'unique': False},
+                {'name': self.lng['result'], 'type': 'string', 'unique': False},
+                {'name': self.lng['periodresult'], 'type': 'string', 'unique': False},
+                {'name': self.lng['comment'], 'type': 'string', 'unique': False},
+                ]
+
+    def _action_load_content(self):
+        return self.show_results()
+
+    def show_results(self):
 
         timeperiod_list = self.dbh.get_timeperiod()
         complete_t_p_result_sum = 0
         complete_t_p_result_count = 0
         complete_t_p_result_average = 0
+
+        result_list = []
+        result_count = 1
+
         for period in timeperiod_list:
             t_p_result_sum = 0
             t_p_result_count = 0
             t_p_result_average = 0
-            self.print_content(root, y_pos, [(period[1], 0)])
-            y_pos += 20
 
-            data_list = [("Date", 0), ("Examtype", 110), ("Result", 100), ("Comment", 40)]
-            self.print_content(root, y_pos, data_list)
-            y_pos += 20
             results = self.dbh.get_exam_result(student_id=self.student_id, subject=self.subject,
                                                timeperiod_id=period[0])
             for r in results:
@@ -122,9 +139,9 @@ class ViewReportStudent(ViewReport):
                 if r.result:
                     t_p_result_sum += r.result * x_t.weight
                     t_p_result_count += x_t.weight
-                data_list = [(r.exam.date, 0), (x_t.name, 110), (r.result, 100), (r.comment, 40)]
-                self.print_content(root, y_pos, data_list)
-                y_pos += 20
+                # print result
+                result_list.append((result_count, r.exam.date, period[1], x_t.name, r.result, "", r.comment))
+                result_count += 1
 
             if t_p_result_count:
                 t_p_result_average = round(float(t_p_result_sum) / t_p_result_count, DIVISOR_PRECISION)
@@ -138,38 +155,46 @@ class ViewReportStudent(ViewReport):
                     complete_t_p_result_count += period[2]
                 else:
                     print("unknown calculation method")
-
-            data_list = [("", 0), ("", 110), (t_p_result_average, 100), ("", 40)]
-            self.print_content(root, y_pos, data_list)
-            y_pos += 20
+            # print Average
+            result_list.append((result_count, "", period[1], "", "", t_p_result_average, ""))
+            result_count += 1
 
         if complete_t_p_result_count:
             complete_t_p_result_average = round(float(complete_t_p_result_sum)/complete_t_p_result_count, DIVISOR_PRECISION)
 
-        data_list = [(self.lng['schoolyear'], 0), ("", 110), (complete_t_p_result_average, 100), ("", 40)]
+        # print schoolyear
+        result_list.append((result_count, "", self.lng['schoolyear'], "", "", complete_t_p_result_average, ""))
+        result_count += 1
 
-        self.print_content(root, y_pos, data_list)
-        y_pos += 20
-
-        return y_pos
+        return result_list
 
 
 class ViewReportSchoolclass(ViewReport):
 
-    def show_results(self, root, y_pos=100):
+    def _define_column_title(self):
+        timeperiod_list = self.dbh.get_timeperiod()
+        column_title = []
+
+        column_title.append({'name': self.lng['student'], 'type': 'string', 'unique': False})
+        for period in timeperiod_list:
+            column_title.append({'name': period[1], 'type': 'string', 'unique': False})
+        column_title.append({'name': self.lng['schoolyear'], 'type': 'string', 'unique': False})
+
+        return column_title
+
+    def _action_load_content(self):
+        return self.show_results()
+
+    def show_results(self):
+
+        result_list = []
+        result_count = 1
 
         timeperiod_list = self.dbh.get_timeperiod()
-        data_list = [(self.lng['student'], 0)]
-        for period in timeperiod_list:
-            data_list.append((period[1], 100))
-        data_list.append((self.lng['schoolyear'], 100))
-
-        self.print_content(root, y_pos, data_list)
-        y_pos += 20
 
         for student in self.dbh.get_students(self.schoolyear, self.schoolclass):
             name = "%s, %s" % (student[1], student[2])
-            data_list = [(name, 0)]
+            data_list = (result_count, name)
             complete_t_p_result_sum = 0
             complete_t_p_result_count = 0
             complete_t_p_result_average = 0
@@ -200,14 +225,14 @@ class ViewReportSchoolclass(ViewReport):
                         complete_t_p_result_count += period[2]
                     else:
                         print("unknown calculation method")
-
-                data_list.append((t_p_result_average, 100))
+                data_list += (t_p_result_average,)
 
             if complete_t_p_result_count:
                 complete_t_p_result_average = round(float(complete_t_p_result_sum)/complete_t_p_result_count, DIVISOR_PRECISION)
-            data_list.append((complete_t_p_result_average, 100))
+            data_list += (complete_t_p_result_average,)
 
-            self.print_content(root, y_pos, data_list)
-            y_pos += 20
+            # print student result
+            result_list.append(data_list)
+            result_count += 1
 
-        return y_pos
+        return result_list
