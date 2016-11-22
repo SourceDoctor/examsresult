143d155a40a4971211091215892560f0505240ba
Thomas Berberich <thomas@walhalla.local>
2016-11-22 02:22:58 +0100

    About Update

--- examsresult/views/about.py
+++ examsresult/views/about.py
@@ -5,7 +5,7 @@ from examsresult.tools import app_icon
 
 class ViewAbout(object):
 
-    def __init__(self, parent, lng, width=400, height=100):
+    def __init__(self, parent, lng, width=400, height=150):
         self.lng = lng
         local_lng = lng['window_about']
 
@@ -22,12 +22,18 @@ class ViewAbout(object):
         app_icon_label.setPixmap(pixmap)
         app_icon_label.move(10, 10)
 
+        version = "%s %s" % (local_lng['version'], "1.0")
+
         title_label = QLabel(self.lng['main']['title'], window)
         title_label.move(50, 10)
-        info_label = QLabel(local_lng['infotext'], window)
+        info_label = QLabel(version, window)
         info_label.move(50, 30)
+        info_label = QLabel(local_lng['infotext'], window)
+        info_label.move(50, 50)
+        info_label = QLabel("created by Thomas Berberich", window)
+        info_label.move(50, 70)
         button = QPushButton(self.lng['main']['ok'], window)
-        button.move(300, 70)
+        button.move(300, 120)
         button.clicked.connect(window.close)
 
         window.exec_()
