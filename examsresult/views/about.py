f76a8b3b8e644ae4ca61260b4782a10c472855f1
Thomas Berberich <thomas@walhalla.local>
2016-12-13 12:13:19 +0100

    Icon Zoom fix

--- examsresult/views/about.py
+++ examsresult/views/about.py
@@ -16,26 +16,24 @@ class ViewAbout(object):
 
         app_icon_label = QLabel(window)
         pixmap = QPixmap(app_icon)
-        # Todo: Zoom Icon
-#         pixmap.scaledToWidth(30)
-#         pixmap.scaledToHeight(30)
+        pixmap = pixmap.scaled(60, 60)
         app_icon_label.setPixmap(pixmap)
-        app_icon_label.move(10, 10)
+        app_icon_label.move(7, 7)
 
         version = "%s %s" % (local_lng['version'], "1.0.1")
 
         title_label = QLabel(self.lng['main']['title'], window)
-        title_label.move(50, 10)
+        title_label.move(80, 10)
         info_label = QLabel(version, window)
-        info_label.move(50, 30)
+        info_label.move(80, 30)
         info_label = QLabel(local_lng['infotext'], window)
-        info_label.move(50, 50)
+        info_label.move(80, 50)
         info_label = QLabel("created by Thomas Berberich", window)
-        info_label.move(50, 70)
+        info_label.move(80, 70)
         supportmail = "program.examsresult@gmail.com"
         info_label = QLabel("E-Mail: <a href=\"mailto:" + supportmail + "?Subject=Support\" target=\"_top\">" + supportmail + "</a>", window)
         info_label.setOpenExternalLinks(True)
-        info_label.move(50, 90)
+        info_label.move(80, 90)
         button = QPushButton(self.lng['main']['ok'], window)
         button.move(300, 120)
         button.clicked.connect(window.close)
